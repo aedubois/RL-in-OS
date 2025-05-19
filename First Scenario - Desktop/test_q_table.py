0ed3abf245d63d55c7a99a25d15c5b570937d701
aedubois <57375545+aedubois@users.noreply.github.com>
2025-05-19 13:11:28 +0200

    Delete useless file

--- First Scenario - Desktop/test_q_table.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-q = np.load("First Scenario - Desktop/q_table.npy")
-print(q.shape)
-#print(q)
-print("Cases non nulles :", np.count_nonzero(q))
-print("Somme Q-table :", np.sum(q))
