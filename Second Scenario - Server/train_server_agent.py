6915d10ce5209fbacbe2ef0a2dfca8b5fa0e3eb8
aedubois <alexandredubois492@gmail.com>
2025-06-20 11:18:19 +0200

    run RL for 100 episodes of 20 steps

--- Second Scenario - Server/train_server_agent.py
+++ Second Scenario - Server/train_server_agent.py
@@ -154,7 +154,7 @@ def plot_rewards(rewards, plots_dir):
     plt.savefig(plot_path)
     print(f"Plot saved as {plot_path}")
 
-def train_agent(num_episodes=2, nb_steps_per_episode=1, sleep_interval=1, return_rewards=False, exploration_rate=1.0):
+def train_agent(num_episodes=100, nb_steps_per_episode=20, sleep_interval=0.1, return_rewards=False, exploration_rate=1.0):
     agent = ServerAgent(exploration_rate=exploration_rate)
     qtable_path = "Second Scenario - Server/q_table_server.npy"
     rewards_dir = "Second Scenario - Server/rewards"
